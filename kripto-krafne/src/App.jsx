abe5ffc75fee8a55b5280dcae8975e44764486dc
leaDugandzic <lea.dugandzic07@gmail.com>
2025-03-23 14:18:49 +0100

    tailwind radiiiiii TOOOOO napravila sam i navbar:)

--- kripto-krafne/src/App.jsx
+++ kripto-krafne/src/App.jsx
@@ -1,8 +1,28 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Logo from './assets/img/logo.png';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Router>
+        <div className="bg-pink-300 p-2 flex justify-between items-center shadow-md">
+          <div className="flex items-center gap-2">
+            <img src={Logo} alt="Logo" className="w-22 h-18" />
+            <Link to="/" className="text-white font-bold italic text-lg">Kripto Krafne</Link>
+          </div>
+          <Link to="/login" className="bg-pink-500 text-white italic px-4 py-2 rounded-full shadow-md hover:bg-pink-600 transition">
+            SignUp/Login
+          </Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
