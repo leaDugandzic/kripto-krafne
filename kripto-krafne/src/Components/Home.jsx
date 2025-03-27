a9210695d664dba978d4dd452981abab03bcf533
leaDugandzic <lea.dugandzic07@gmail.com>
2025-03-27 18:55:44 +0100

    json data added

--- kripto-krafne/src/Components/Home.jsx
+++ kripto-krafne/src/Components/Home.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import CategoryList from './CategoryList';
 
 const Home = () => {
     const images = import.meta.glob('/src/assets/img/kutije/*.png', { eager: true });
     const imageArray = Object.values(images).map((img) => img.default);
     return (
-        <div className="w-250 mx-auto mt-15 flex flex-wrap justify-center gap-7 boxes-container">
+        <div className="w-250 mx-auto mt-10 flex flex-wrap justify-center gap-7 boxes-container">
             {imageArray.map((src, index) => (
                 <Link to={`/box/${index}/level/1`}>
                     <img key={index} src={src} alt={`Krafna ${index}`} className="w-70 h-70 box-image" />
                 </Link>
             ))}
+            {/* <CategoryList></CategoryList> */}
         </div>
 
     )
