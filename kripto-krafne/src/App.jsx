b58f86c245dcbfbd5958dd8e74d4e82e3de718c1
anaSimovic <anasimovic092@gmail.com>
2025-04-01 21:50:12 +0200

    Images komponenta,no kod jos u slici dodat cu kad odlucimo

--- kripto-krafne/src/App.jsx
+++ kripto-krafne/src/App.jsx
@@ -12,6 +12,7 @@ import levels from './library/levels.json';
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 
+import Images from './KraljestvoKrafni/Images'
 import Radnici from './KraljestvoKrafni/Radnici';
 import Glazba from './KraljestvoKrafni/Glazba';
 import { Menu } from 'lucide-react';
@@ -57,39 +58,35 @@ function App() {
     <div className='body'>
       <Router>
 
-        <div className=" navbar bg-pink-300 p-2 flex flex-row justify-between items-center shadow-md">
+        <div className="navbar bg-pink-300 p-2 flex flex-row justify-between items-center shadow-md">
           <div className="flex items-center gap-2">
             <img src={Logo} alt="Logo" className="w-22 h-18" />
             <Link to="/" className="text-white font-bold italic text-lg">Kripto Krafne</Link>
           </div>
 
-          <div className="flex flex-row" id="buttons" style={{ display: stylebtn }}>
-            <Link to="/signup" className="bg-pink-500 text-white italic px-4 py-2 rounded-full shadow-md hover:bg-pink-600 transition">
-              <div>SignUp</div>
+          <div className="flex flex-row gap-4" id="buttons" style={{ display: stylebtn }}>
+            <Link to="/signup" className="bg-pink-500 text-white italic px-6 py-2 rounded-full shadow-md hover:bg-pink-600 transition-all">
+              SignUp
             </Link>
-            <Link to="/login" className="bg-pink-500 text-white italic px-4 py-2 rounded-full shadow-md hover:bg-pink-600 transition">
-              <div>Login</div>
+            <Link to="/login" className="bg-pink-500 text-white italic px-6 py-2 rounded-full shadow-md hover:bg-pink-600 transition-all">
+              Login
             </Link>
           </div>
 
           <div id="buttons" style={{ display: stylepfp }}>
             <div className="dropdown dropdown-end">
-              <div tabIndex={0} role="button" className=" btn-ghost btn-circle avatar">
+              <div tabIndex={0} role="button" className="btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
                   <img className="w-[50px]" src={Krafnapfp}></img>
-
                 </div>
               </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-pink-500 rounded-box z-1 mt-3 w-52 p-2 text-[15px] font-bold shadow">
+              <ul tabIndex={0} className="menu menu-sm dropdown-content bg-pink-500 rounded-box z-1 mt-3 w-52 p-2 text-[15px] font-bold shadow">
                 <li><a onClick={handleLogout}>Logout</a></li>
               </ul>
             </div>
-
           </div>
-
         </div>
+
 
         <Routes>
           <Route path="/" element={<Home />} />
@@ -98,6 +95,8 @@ function App() {
           <Route path="/radnici" element={<Radnici></Radnici>}></Route>
           <Route path="/glazba" element={<Glazba></Glazba>}></Route>
           <Route path="/menu" element={<Menu></Menu>}></Route>
+          <Route path="/images" element={<Images />}></Route>
+
 
           <Route path="/box/:id" element={<Level levels={levels} />} />
         </Routes>
