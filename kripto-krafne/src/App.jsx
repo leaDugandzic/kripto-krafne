a177e7297d9304abec7a289d16610784e1a2fffd
ana <anacikes00@gmail.com>
2025-11-27 10:30:30 +0100

    Strvorila Navbar.jsx

--- kripto-krafne/src/App.jsx
+++ kripto-krafne/src/App.jsx
@@ -21,26 +21,10 @@ import ReverseEngineeringChallenge from './KraljestvoKrafni/ReverseEngineering';
 import DonutGame from './KraljestvoKrafni/DonutGame';
 import AIChatbot from './Components/AiChatbot';
 import chatbotIcon from "./assets/img/chatbotIcon.png";
+import Navbar from './Components/Navbar';
 function App() {
-  const [stylepfp, setStylepfp] = useState("none");
-  const [stylebtn, setStylebtn] = useState("flex");
-  const[chatbotClick, setChatboxClick] = useState(false);
-
   
- useEffect(() => {
-    fetch("http://localhost/kripto-krafne/kripto-krafne/src/backend/session.php", {
-      method: "GET",
-      credentials: "include",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.loggedIn) {
-          setStylepfp("block");
-          setStylebtn("none");
-        }
-      })
-      .catch((err) => console.error("Session check error:", err));
-  }, []);
+  const[chatbotClick, setChatboxClick] = useState(false);
 
 
 
@@ -48,36 +32,8 @@ function App() {
     <div className='body'>
 
       <Router>
-
-        <div className="navbar bg-pink-300 p-2 flex flex-row justify-between items-center shadow-md">
-          <div className="flex items-center gap-2">
-            <img src={Logo} alt="Logo" className="w-22 h-18" />
-            <Link to="/" className="text-white font-bold title-font small">Kripto Krafne</Link>
-          </div>
-
-          <div className="flex flex-row gap-4" id="buttons" style={{ display: stylebtn }}>
-            <Link to="/signup" className="bg-pink-500 text-white italic px-6 py-2 rounded-full shadow-md hover:bg-pink-600 transition-all">
-              SignUp
-            </Link>
-            <Link to="/login" className="bg-pink-500 text-white italic px-6 py-2 rounded-full shadow-md hover:bg-pink-600 transition-all">
-              Login
-            </Link>
-          </div>
-
-          <div id="buttons" style={{ display: stylepfp }}>
-            <div className="dropdown dropdown-end">
-              <div tabIndex={0} role="button" className="btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <img className="w-[50px]" src={Krafnapfp}></img>
-                </div>
-              </div>
-              <ul tabIndex={0} className="menu menu-sm dropdown-content bg-pink-500 rounded-box z-1 mt-3 w-52 p-2 text-[15px] font-bold shadow">
-                <li><a >Logout</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
+      <Navbar></Navbar>
+      
         <div onClick={()=>setChatboxClick(true)}className='fixed bottom-0 right-0 flex items-center justify-center p-[20px] hover:scale-110 transition-transform duration-300 ease-in-out'>
           <img src={chatbotIcon} className='w-[100px]'></img>
         </div>
